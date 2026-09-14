fc9ab495b27cd08f6ac7247efe86c8a48eaa2485
Anita Hall <anitahall102790@gmail.com>
2026-09-14 19:53:13 -0400

    configure Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { resolve } from 'path'
 
 export default {
+  base: '/portfolio/',
+
   root: resolve(__dirname, 'src'),
+
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        about: resolve(__dirname, 'src/about.html'),
+        work: resolve(__dirname, 'src/work.html'),
+        contact: resolve(__dirname, 'src/contact.html')
+      }
+    }
   }
 }
