ba1b8e3b4c2e3e50aed62e532c66a7739082c2d3
Anita Hall <anitahall102790@gmail.com>
2026-09-15 08:24:35 -0400

    Added additional pages to vite configuration,fixed left/right scroll issue, fixed resume link

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default {
   base: '/portfolio/',
 
   root: resolve(__dirname, 'src'),
+  publicDir: resolve(__dirname, 'public'),
 
   build: {
     outDir: '../dist',
@@ -13,7 +14,22 @@ export default {
         main: resolve(__dirname, 'src/index.html'),
         about: resolve(__dirname, 'src/about.html'),
         work: resolve(__dirname, 'src/work.html'),
-        contact: resolve(__dirname, 'src/contact.html')
+        contact: resolve(__dirname, 'src/contact.html'),
+      
+        azureModernization: resolve(
+          __dirname,
+          'src/case-studies/azure-modernization.html'
+        ),
+      
+        documentValidation: resolve(
+          __dirname,
+          'src/case-studies/document-validation.html'
+        ),
+      
+        aiAdoptionStrategy: resolve(
+          __dirname,
+          'src/case-studies/ai-adoption-strategy.html'
+        )
       }
     }
   }
